2a360ae928a8e865c7103eb31460f76f35c82391
Batuhan Hesapdar <batuhanhesapdar@hotmail.com>
2022-08-16 21:58:48 +0300

    LevelManager, EventManager and SwipeDetector classes are added

---
+++ BathroomSelfie/Assets/Scripts/Arrow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ArrowDirection
+{
+    Up,
+    Down,
+    Right,
+    Left
+}
+public class Arrow : MonoBehaviour
+{
+    public ArrowDirection myArrowDirection;
+
+}
